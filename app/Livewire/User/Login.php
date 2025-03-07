daba9532b8ba367cb2f7eaaf01f2f2337a2d2b04
Samuel Dias <samuelcarvalhodias2004@gmail.com>
2025-03-07 12:26:45 -0300

    Salvamento dos dados do usuário na session

--- app/Livewire/User/Login.php
+++ app/Livewire/User/Login.php
@@ -31,14 +31,17 @@ class Login extends Component
 
         // Verifica o token com o método do Firebase
         $verifiedIdToken = $auth->verifyIdToken($idToken,false,180);
-
+        
+        // Identifica o usuário
         $uid = $verifiedIdToken->claims()->get('sub');
 
-        
+        // Coleta informações do usuário no firebase
         $user = $auth->getUser($uid);
         
-        dd($user);
-        
+        // Salva informações do usuário na sessão
+        session(['user' => $user]);
+
+        dd($user); 
 
     }
 
